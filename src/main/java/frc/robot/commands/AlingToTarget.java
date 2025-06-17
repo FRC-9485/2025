511d2781350a763d72d49ce3401d3bd5853c6c58
FRC-9485 <frcsesilinhares@gmail.com>
2025-06-17 15:31:39 -0300

    alterações na limelight

--- src/main/java/frc/robot/commands/AlingToTarget.java
+++ src/main/java/frc/robot/commands/AlingToTarget.java
@@ -10,12 +10,14 @@ import frc.robot.subsystems.Limelight;
 import frc.robot.subsystems.SwerveSubsystem;
 
 public class AlingToTarget extends Command {
-
+    
     private final Limelight limelight;
     private Translation2d translation;
     private final PIDController xController;
     private final SwerveSubsystem subsystem;
     private final PIDController rotationController;
+    private double setpointY;
+    private double setpointX;
     
     // Constantes para ajuste fino
     private static final double kP_ROTATION = 0.1;
@@ -23,27 +25,25 @@ public class AlingToTarget extends Command {
     private static final double kD_ROTATION = 0.005;
     private static final double TOLERANCIA_ROTATION = 0.2;
     
-    private static final double kP_X = 0.1;  // Ajuste esses valores conforme necessário
+    private static final double kP_X = 0.11;  // Ajuste esses valores conforme necessário
     private static final double kI_X = 0.01;
     private static final double kD_X = 0.005;
     private static final double TOLERANCIA_X = 0.1;
-
+    
     // private static final double TOLERANCIA_ALINHAMENTO = 0.6;
-    private double setpointY;
-    private double setpointX;
     private static final int MAX_TENTATIVAS = 3;
     private static final double MAX_CORRECAO = 8;
     private static final double ZONA_MORTA = 0.05;
     private static final double MAX_VELOCIDADE_X = 8; 
     private static final double ANGULO_MAXIMO = 30.0;
     private static final double TIMEOUT_SECONDS = 5.5;
+    private static final double TEMPO_MINIMO_ESTAVEL = 0.5;
     
-    private final Timer timer = new Timer();
     private int tentativas = 0;
     private boolean ultimoEstadoTarget = false;
     private double ultimaTx = 0.0;
     private double ultimoTempoMudanca = 0.0;
-    private static final double TEMPO_MINIMO_ESTAVEL = 0.5;
+    private final Timer timer = new Timer();
     
     public AlingToTarget(Limelight limelight, SwerveSubsystem subsystem, double setpointX, double setpointY) {
         if (limelight == null || subsystem == null) {
@@ -144,10 +144,10 @@ public class AlingToTarget extends Command {
                         ta = 4;
                     }
 
-                    // Aumenta a velocidade dependendo do TA
-                    correcaoRotacao = -correcaoRotacao * ta;
-                    
-                    translation = new Translation2d(-correcaoX * ta, 0);
+                    correcaoRotacao = -correcaoRotacao;
+                    correcaoX = -correcaoX;
+
+                    translation = new Translation2d(correcaoX * ta, 0);
                     subsystem.drive(translation, correcaoRotacao, true);
                     
                     // Debug
